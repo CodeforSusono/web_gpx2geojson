c875599e31b5cf935c68e61336dd6759954dadf6
Akira <suntomoon@gmail.com>
2020-01-27 21:55:49 +0900

    first commit
    add the function to upload gpx file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # web_gpx2geojson
+
+web version of gpx2geojson (https://github.com/aktnk/umap_geojson.git)
+
---
+++ view.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_DIR = os.getenv('UPLOAD_DIR')
+ALLOWED_EXTENSIONS = {'gpx'}
+
+app = Flask(__name__)
+app.config['UPLOAD_DIR'] = UPLOAD_DIR
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template('index.html', flg=None, alert='アップロードするファイルを選択し、送信してください')
+    if request.method == 'POST':
+        print(request.files)
+        if 'inputFile' not in request.files:
+            return render_template('index.html', flg=False, alert='アップロードするファイルが指定されていません')
+        file = request.files['inputFile']
+        if file and allowed_file(file.filename):
+            filename = os.path.join(app.config['UPLOAD_DIR'], secure_filename(file.filename))
+            file.save(filename)
+            return render_template('index.html', flg=True, alert='ファイル　%s をアップロードしました' % filename)
+        else:
+            return render_template('index.html', flg=False, alert='.gpxファイル以外は処理できません')
+
+if __name__=='__main__':
+    app.run(debug=True)
